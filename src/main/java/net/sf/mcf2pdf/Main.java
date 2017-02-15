6f0e4b0dd96d404590d106d1d58b65c9ea4c3fc6
Mimoza <mimoza@zhext.tk>
2017-02-15 15:42:36 +0100

    Remaniement du Main

--- src/main/java/net/sf/mcf2pdf/Main.java
+++ src/main/java/net/sf/mcf2pdf/Main.java
@@ -3,14 +3,7 @@
  *******************************************************************************/
 package net.sf.mcf2pdf;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
 import net.sf.mcf2pdf.mcfelements.util.PdfUtil;
-
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -26,6 +19,12 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 
 /**
  * Main entry point of the mcf2pdf application. Creates an
@@ -37,8 +36,10 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class Main {
 
-	@SuppressWarnings("static-access")
-	public static void main(String[] args) {
+	private static CommandLine commandLine = initCommandLine();
+	private  static final Options OPTIONS = initOptions();
+
+	private static Options initOptions(){
 		Options options = new Options();
 
 		Option o = OptionBuilder.hasArg().isRequired().withDescription("Installation location of My CEWE Photobook. REQUIRED.").create('i');
@@ -52,41 +53,50 @@ public class Main {
 		options.addOption("q", false, "Quiet mode - only errors are logged.");
 		options.addOption("d", false, "Enables debugging logging output.");
 
-		CommandLine cl;
+		return options;
+	}
+
+	private static CommandLine initCommandLine(String... args){
 		try {
 			CommandLineParser parser = new PosixParser();
-			cl = parser.parse(options, args);
+			return parser.parse(OPTIONS, args);
 		}
 		catch (ParseException pe) {
-			printUsage(options, pe);
+			printUsage(pe);
+			System.exit(3);
+			return null;
+		}
+	}
+
+	@SuppressWarnings("static-access")
+	public static void main(String... args) {
+
+		commandLine = initCommandLine(args);
+
+		if (commandLine.hasOption("h")) {
+			printUsage(null);
+			return;
+		}
+
+		if (commandLine.getArgs().length != 2) {
+			printUsage(new ParseException("INFILE and OUTFILE must be specified. Arguments were: " + commandLine.getArgList()));
 			System.exit(3);
 			return;
 		}
 
-		if (cl.hasOption("h")) {
-			printUsage(options, null);
-			return;
-		}
-
-		if (cl.getArgs().length != 2) {
-			printUsage(options, new ParseException("INFILE and OUTFILE must be specified. Arguments were: " + cl.getArgList()));
-			System.exit(3);
-			return;
-		}
-
-		File installDir = new File(cl.getOptionValue("i"));
+		File installDir = new File(commandLine.getOptionValue("i"));
 		if (!installDir.isDirectory()) {
-			printUsage(options, new ParseException("Specified installation directory does not exist."));
+			printUsage(new ParseException("Specified installation directory does not exist."));
 			System.exit(3);
 			return;
 		}
 
-		File tempDir = null;
-		String sTempDir = cl.getOptionValue("t");
+		File tempDir;
+		String sTempDir = commandLine.getOptionValue("t");
 		if (sTempDir == null) {
 			tempDir = new File(new File(System.getProperty("user.home")), ".mcf");
 			if (!tempDir.isDirectory()) {
-				printUsage(options, new ParseException("MCF temporary location not specified and default location " + tempDir + " does not exist."));
+				printUsage(new ParseException("MCF temporary location not specified and default location " + tempDir + " does not exist."));
 				System.exit(3);
 				return;
 			}
@@ -94,63 +104,43 @@ public class Main {
 		else {
 			tempDir = new File(sTempDir);
 			if (!tempDir.isDirectory()) {
-				printUsage(options, new ParseException("Specified temporary location does not exist."));
+				printUsage(new ParseException("Specified temporary location does not exist."));
 				System.exit(3);
 				return;
 			}
 		}
 
-		File mcfFile = new File(cl.getArgs()[0]);
+		File mcfFile = new File(commandLine.getArgs()[0]);
 		if (!mcfFile.isFile()) {
-			printUsage(options, new ParseException("MCF input file does not exist."));
+			printUsage(new ParseException("MCF input file does not exist."));
 			System.exit(3);
 			return;
 		}
 		mcfFile = mcfFile.getAbsoluteFile();
 
 		File tempImages = new File(new File(System.getProperty("user.home")), ".mcf2pdf");
-		if (cl.hasOption("w")) {
-			tempImages = new File(cl.getOptionValue("w"));
+		if (commandLine.hasOption("w")) {
+			tempImages = new File(commandLine.getOptionValue("w"));
 			if (!tempImages.mkdirs() && !tempImages.isDirectory()) {
-				printUsage(options, new ParseException("Specified working dir does not exist and could not be created."));
+				printUsage(new ParseException("Specified working dir does not exist and could not be created."));
 				System.exit(3);
 				return;
 			}
 		}
 
-		int dpi = 150;
-		if (cl.hasOption("r")) {
-			try {
-				dpi = Integer.valueOf(cl.getOptionValue("r")).intValue();
-				if (dpi < 30 || dpi > 600)
-					throw new IllegalArgumentException();
-			}
-			catch (Exception e) {
-				printUsage(options, new ParseException("Parameter for option -r must be an integer between 30 and 600."));
-			}
-		}
+		int dpi = checkArgument("r",600,30, 150, "Parameter for option -r must be an integer between 30 and 600.");
 
-		int maxPageNo = -1;
-		if (cl.hasOption("n")) {
-			try {
-				maxPageNo = Integer.valueOf(cl.getOptionValue("n")).intValue();
-				if (maxPageNo < 0)
-					throw new IllegalArgumentException();
-			}
-			catch (Exception e) {
-				printUsage(options, new ParseException("Parameter for option -n must be an integer >= 0."));
-			}
-		}
+		int maxPageNo = checkArgument("n",Integer.MAX_VALUE, 0, -1,"Parameter for option -n must be an integer >= 0.");
 
 		OutputStream finalOut;
-		if (cl.getArgs()[1].equals("-"))
+		if ("-".equals(commandLine.getArgs()[1]))
 			finalOut = System.out;
 		else {
 			try {
-				finalOut = new FileOutputStream(cl.getArgs()[1]);
+				finalOut = new FileOutputStream(commandLine.getArgs()[1]);
 			}
 			catch (IOException e) {
-				printUsage(options, new ParseException("Output file could not be created."));
+				printUsage(new ParseException("Output file could not be created."));
 				System.exit(3);
 				return;
 			}
@@ -161,28 +151,28 @@ public class Main {
 			PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
 
 			Logger.getRootLogger().setLevel(Level.INFO);
-			if (cl.hasOption("q"))
+			if (commandLine.hasOption("q"))
 				Logger.getRootLogger().setLevel(Level.ERROR);
-			if (cl.hasOption("d"))
+			if (commandLine.hasOption("d"))
 				Logger.getRootLogger().setLevel(Level.DEBUG);
 		}
 
 		// start conversion to XSL-FO
 		// if -x is specified, this is the only thing we do
 		OutputStream xslFoOut;
-		if (cl.hasOption("x"))
+		if (commandLine.hasOption("x")) {
 			xslFoOut = finalOut;
-		else
+		}else {
 			xslFoOut = new ByteArrayOutputStream();
+		}
 
 		Log log = LogFactory.getLog(Main.class);
 
 		try {
-			new Mcf2FoConverter(installDir, tempDir, tempImages).convert(
-					mcfFile, xslFoOut, dpi, maxPageNo);
+			new Mcf2FoConverter(installDir, tempDir, tempImages).convert(mcfFile, xslFoOut, dpi, maxPageNo);
 			xslFoOut.flush();
 
-			if (!cl.hasOption("x")) {
+			if (!commandLine.hasOption("x")) {
 				// convert to PDF
 				log.debug("Converting XSL-FO data to PDF");
 				byte[] data = ((ByteArrayOutputStream)xslFoOut).toByteArray();
@@ -202,13 +192,29 @@ public class Main {
 		}
 	}
 
-	private static void printUsage(Options options, ParseException pe) {
-		if (pe != null)
+	private static int checkArgument(String optionName, int max, int min, int defaultValue, String errorMessage){
+		int argumentValue = defaultValue;
+		if (commandLine.hasOption(optionName)) {
+			try {
+				argumentValue = Integer.parseInt(commandLine.getOptionValue(optionName));
+				if (argumentValue < min || argumentValue > max){
+					throw new IllegalArgumentException(errorMessage);
+				}
+			} catch (IllegalArgumentException e) {
+				printUsage(new ParseException(errorMessage));
+			}
+		}
+		return argumentValue;
+	}
+
+	private static void printUsage(ParseException pe) {
+		if (pe != null){
 			System.err.println("ERROR: " + pe.getMessage());
+		}
 		System.out.println();
 		System.out.println("mcf2pdf My CEWE Photobook to PDF converter");
 		HelpFormatter hf = new HelpFormatter();
-		hf.printHelp("mcf2pdf <options> INFILE OUTFILE", "Options are:", options,
+		hf.printHelp("mcf2pdf <OPTIONS> INFILE OUTFILE", "Options are:", OPTIONS,
 				"If -t is not specified, <USER_HOME>/.mcf is used.\n" +
 				"If -w is not specified, <USER_HOME>/.mcf2pdf is created and used.\n" +
 				"If you specify a dash (-) as OUTFILE, resulting content will be written to STDOUT. Notice that, in that case, temporary image files will be kept in specified image working directory. Notice also that you should add option -q in this case to avoid logging output.");
