7edaa145785d684ec8c1b3d4fcd647414011f07a
Mimoza <github@zhext.tk>
2017-04-07 14:32:41 +0200

    Amélioration du UTF8 Control

--- src/main/java/net/sf/mcf2pdf/gui/i18n/UTF8Control.java
+++ src/main/java/net/sf/mcf2pdf/gui/i18n/UTF8Control.java
@@ -6,6 +6,27 @@
  * file for more details.
  */
 
+/*
+ * Copyright (c) 1996, 2013, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ */
+
+/*
+ * (C) Copyright Taligent, Inc. 1996, 1997 - All Rights Reserved
+ * (C) Copyright IBM Corp. 1996 - 1999 - All Rights Reserved
+ *
+ * The original version of this source code and documentation
+ * is copyrighted and owned by Taligent, Inc., a wholly-owned
+ * subsidiary of IBM. These materials are provided under terms
+ * of a License Agreement between Taligent and Sun. This technology
+ * is protected by multiple US and International patents.
+ *
+ * This notice and attribution to Taligent may not be removed.
+ * Taligent is a registered trademark of Taligent, Inc.
+ *
+ */
+
 package net.sf.mcf2pdf.gui.i18n;
 
 import java.io.IOException;
@@ -13,45 +34,88 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 
 /**
- * It's a copy from :
+ * Widely inspired from the method of :
  * http://stackoverflow.com/questions/4659929/how-to-use-utf-8-in-resource-properties-with-resourcebundle
  */
 public class UTF8Control extends ResourceBundle.Control {
 	@Override
-	public ResourceBundle newBundle (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
-		throws IllegalAccessException, InstantiationException, IOException	{
-
-		// The below is a copy of the default implementation.
+	public ResourceBundle newBundle(String baseName, Locale locale, String format,
+									ClassLoader loader, boolean reload)
+		throws IllegalAccessException, InstantiationException, IOException {
 		String bundleName = toBundleName(baseName, locale);
-		String resourceName = toResourceName(bundleName, "properties");
 		ResourceBundle bundle = null;
-		InputStream stream = null;
-		if (reload) {
-			URL url = loader.getResource(resourceName);
-			if (url != null) {
-				URLConnection connection = url.openConnection();
-				if (connection != null) {
-					connection.setUseCaches(false);
-					stream = connection.getInputStream();
+		if ("java.class".equals(format)) {
+			try {
+				@SuppressWarnings("unchecked")
+				Class<? extends ResourceBundle> bundleClass
+					= (Class<? extends ResourceBundle>)loader.loadClass(bundleName);
+
+				// If the class isn't a ResourceBundle subclass, throw a
+				// ClassCastException.
+				if (ResourceBundle.class.isAssignableFrom(bundleClass)) {
+					bundle = bundleClass.newInstance();
+				} else {
+					throw new ClassCastException(bundleClass.getName() + " cannot be cast to ResourceBundle");
+				}
+			} catch (ClassNotFoundException ignored) {}
+		} else if ("java.properties".equals(format)) {
+			final String resourceName = toResourceName0(bundleName, "properties");
+			if (resourceName == null) {
+				return bundle;
+			}
+			final ClassLoader classLoader = loader;
+			InputStream stream;
+			try {
+				final boolean reloadFlag = reload;
+				stream = AccessController.doPrivileged((PrivilegedExceptionAction<InputStream>) () -> {
+						InputStream is = null;
+						if (reloadFlag) {
+							URL url = classLoader.getResource(resourceName);
+							if (url != null) {
+								URLConnection connection = url.openConnection();
+								if (connection != null) {
+									// Disable caches to get fresh data for
+									// reloading.
+									connection.setUseCaches(false);
+									is = connection.getInputStream();
+								}
+							}
+						} else {
+							is = classLoader.getResourceAsStream(resourceName);
+						}
+						return is;
+					});
+			} catch (PrivilegedActionException e) {
+				throw (IOException) e.getException();
+			}
+			if (stream != null) {
+				try {
+					bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+				} finally {
+					stream.close();
 				}
 			}
 		} else {
-			stream = loader.getResourceAsStream(resourceName);
-		}
-		if (stream != null) {
-			try {
-				// Only this line is changed to make it to read properties files as UTF-8.
-				bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
-			} finally {
-				stream.close();
-			}
+			throw new IllegalArgumentException("unknown format: " + format);
 		}
 		return bundle;
+	}
+
+	private String toResourceName0(String bundleName, String suffix) {
+		// application protocol check
+		if (bundleName.contains("://")) {
+			return null;
+		} else {
+			return toResourceName(bundleName, suffix);
+		}
 	}
 }
